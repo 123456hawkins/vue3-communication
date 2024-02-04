5d2516b72448a70385a3e38f6c95239fc6f6e3c6
123456hawkins <2398430768@qq.com>
2024-02-04 14:36:14 +0800

    新增useAttrs

--- src/views/04_v-model/Child1.vue
+++ src/views/04_v-model/Child1.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="child2">
     <h1>同时绑定多个v-model</h1>
-    <button @click="handler">pageNo{{ pageNo }}</button>
-    <button @click="$emit('update:pageSize', pageSize + 4)">
-      pageSize{{ pageSize }}
-    </button>
+    <button @click="handler1">pageNo{{ pageNo }}</button>
+    <button @click="handler2">pageSize{{ pageSize }}</button>
   </div>
 </template>
 
 <script setup lang="ts">
-let props = defineProps(["pageNo", "pageSize"]);
-let $emit = defineEmits(["update:pageNo", "update:pageSize"]);
-//第一个按钮的事件回调
-const handler = () => {
-  $emit("update:pageNo", props.pageNo + 3);
-};
+const props = defineProps(['pageNo', 'pageSize'])
+const $emit = defineEmits(['update:pageNo', 'update:pageSize'])
+const handler1 = () => {
+  $emit('update:pageNo', props.pageNo + 4)
+}
+const handler2 = () => {
+  $emit('update:pageSize', props.pageSize + 6)
+}
 </script>
 
 <style scoped>
