5f52d555068f0a6826c0bc499707148fc591426a
123456hawkins <2398430768@qq.com>
2024-02-04 15:58:04 +0800

    新增provide/inject

--- src/views/06_ref-children-parent/Daughter.vue
+++ src/views/06_ref-children-parent/Daughter.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="dau">
-     <h1>我是闺女曹杰{{money}}</h1>
-     <button @click="handler($parent)">点击我爸爸给我10000元</button>
+    <h1>我是闺女曹节{{ money }}</h1>
+    <button @click="handler($parent)">点击我爸爸给我10000元</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
+import { ref } from 'vue'
 //闺女钱数
-let money = ref(999999);
+let money = ref(999999)
 //闺女按钮点击回调
-const handler = ($parent)=>{
-   money.value+=10000;
-   $parent.money-=10000;
+const handler = ($parent) => {
+  console.log('自动注入的$parent', $parent)
+
+  money.value += 10000
+  $parent.money -= 10000
 }
 </script>
 
 <style scoped>
-.dau{
+.dau {
   width: 300px;
   height: 300px;
   background: hotpink;
--- src/views/07_provide-inject/ProvideInjectTest.vue
+++ src/views/07_provide-inject/ProvideInjectTest.vue
@@ -14,7 +14,8 @@ let car = ref("法拉利");
 //祖先组件给后代组件提供数据
 //两个参数:第一个参数就是提供的数据key
 //第二个参数:祖先组件提供数据
-provide("TOKEN", car);
+// provide("TOKEN", car);
+provide("TOKEN",car)
 </script>
 
 <style scoped>
